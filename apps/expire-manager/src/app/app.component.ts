44316fc70dfcc0187b4451ef8d9ad544cf0fd525
fasapollo <fas.apollo@hotmail.com>
2022-07-25 19:46:24 -0300

    initial commit

---
+++ apps/expire-manager/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bullsoft-frontend-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'expire-manager';
+}
